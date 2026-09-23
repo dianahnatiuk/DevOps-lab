887ee0b5e93aa9b1dde1266f2a775f7a6073f4af
diana hnatiuk <dianagnat24@gmail.com>
2026-09-23 18:46:10 +0300

    dd  pipeline

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,19 @@
 from fastapi.testclient import TestClient
-from main import app
+
+from app.main import app
 
 client = TestClient(app)
+
 
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "DevOps Lab 1 API is running"}
+
+
+def test_create_item():
+    response = client.post("/items/", params={"name": "test-item"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "test-item"
+    assert "id" in data
